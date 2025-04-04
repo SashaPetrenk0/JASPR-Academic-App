05f349e5771bd900bd011ed3b64d53108ba85c1f
Sasha <sasha.petrenk6@gmail.com>
2025-04-04 11:22:12 +1000

    Create quiz class

---
+++ src/main/java/org/jaspr/hr/demo/Quiz.java
@@ -0,0 +1,57 @@
+package org.jaspr.hr.demo;
+
+public class Quiz {
+    private int id;
+    private String title;
+    private String description;
+    private String topic;
+    private int numOfQuestions;
+
+
+    public Quiz(String title, String description, String topic, int numOfQuestions){
+        this.title = title;
+        this.description = description;
+        this.topic = topic;
+        this.numOfQuestions = numOfQuestions;
+
+    }
+
+    public int getId() {
+        return id;
+    }
+    private void setID(int id){
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getTopic() {
+        return topic;
+    }
+
+    public int getNumOfQuestions() {
+        return numOfQuestions;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setNumOfQuestions(int numOfQuestions) {
+        this.numOfQuestions = numOfQuestions;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
+}
