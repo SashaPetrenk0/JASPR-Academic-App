b89f4b0e4b1f34f317541019c0cbe2d2d4118cd2
Pragati Vijayakumar <n11171499@qut.edu.au>
2025-04-20 09:00:30 +1000

    Create function to handle the submit button being clicked.
    Input fields are linked to a Student instance to create a new Student.

--- src/main/java/org/jaspr/hr/demo/StudentController.java
+++ src/main/java/org/jaspr/hr/demo/StudentController.java
@@ -20,4 +20,17 @@ public class StudentController {
     private TextField passwordField;
 
     private Student currentStudent;
+
+    @FXML
+    private void onSubmitClicked(){
+        String name = nameField.getText();
+        int age;
+        age = Integer.parseInt(ageField.getText());
+        String email = emailField.getText();
+        String password = passwordField.getText();
+
+        currentStudent = new Student(name, age, email, password);
+
+        // TODO: Error handling for incorrect user inputs
+    }
 }
