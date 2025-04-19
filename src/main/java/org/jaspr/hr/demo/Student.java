fcbb6e4248390300ac6b29eb50934be69279cda9
Pragati Vijayakumar <n11171499@qut.edu.au>
2025-04-20 08:31:38 +1000

    Create Student class constructor with name, age, email and password parameters.

--- src/main/java/org/jaspr/hr/demo/Student.java
+++ src/main/java/org/jaspr/hr/demo/Student.java
@@ -1,4 +1,15 @@
 package org.jaspr.hr.demo;
 
 public class Student {
+    private String name;
+    private int age;
+    private String email;
+    private String password;
+
+    public Student(String name, int age, String email, String password){
+        this.name = name;
+        this.age = age;
+        this.email = email;
+        this.password = password;
+    }
 }
