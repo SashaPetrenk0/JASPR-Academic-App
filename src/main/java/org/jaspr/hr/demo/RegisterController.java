03ba0ff1c9d275c869f6d6cff51957533ea8652e
Pragati Vijayakumar <n11171499@qut.edu.au>
2025-04-20 13:08:05 +1000

    Initially set all registration form's visibility to false. Created a switch-case to set the form correlating to the user type as visible.

--- src/main/java/org/jaspr/hr/demo/RegisterController.java
+++ src/main/java/org/jaspr/hr/demo/RegisterController.java
@@ -1,4 +1,38 @@
 package org.jaspr.hr.demo;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class RegisterController {
+    @FXML
+    private ComboBox<String> roleComboBox;
+    @FXML
+    private VBox studentForm, teacherForm, parentForm, adminForm;
+
+    @FXML
+    private void onRoleSelected(){
+        String role = roleComboBox.getValue();
+        studentForm.setVisible(false);
+        teacherForm.setVisible(false);
+        parentForm.setVisible(false);
+        adminForm.setVisible(false);
+
+        switch(role){
+            case "Student" -> studentForm.setVisible(true);
+            case "Teacher" -> studentForm.setVisible(true);
+            case "Parent" -> studentForm.setVisible(true);
+            case "Admin" -> studentForm.setVisible(true);
+        }
+    }
+
 }
