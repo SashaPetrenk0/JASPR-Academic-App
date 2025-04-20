b08c14ce50e0f857f2768324e5d7c0a72b3bc259
Pragati Vijayakumar <n11171499@qut.edu.au>
2025-04-20 20:11:03 +1000

    Add studentID field.

--- src/main/java/org/jaspr/hr/demo/Student.java
+++ src/main/java/org/jaspr/hr/demo/Student.java
@@ -3,10 +3,12 @@ package org.jaspr.hr.demo;
 public class Student implements User{
     private String name, email, password;
     private int age;
+    private int studentID;
 
-    public Student(String name, int age, String email, String password){
+    public Student(String name, int age, int studentID, String email, String password){
         this.name = name;
         this.age = age;
+        this.studentID = studentID;
         this.email = email;
         this.password = password;
     }
@@ -18,6 +20,11 @@ public class Student implements User{
     public int getAge() {
         return age;
     }
+
+    public int getStudentID() {
+        return studentID;
+    }
+
 
     public String getEmail() {
         return email;
