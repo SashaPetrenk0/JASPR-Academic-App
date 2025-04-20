f3a4fffdcfeb6024378f09de60158a570eda60d6
Pragati Vijayakumar <n11171499@qut.edu.au>
2025-04-20 12:14:50 +1000

    Adding a success message for successful student signup.

--- src/main/java/org/jaspr/hr/demo/StudentController.java
+++ src/main/java/org/jaspr/hr/demo/StudentController.java
@@ -4,7 +4,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.event.ActionEvent;
-
+import javafx.scene.control.Label;
 public class StudentController {
 
     @FXML
@@ -19,20 +19,35 @@ public class StudentController {
     @FXML
     private TextField passwordField;
 
+    @FXML
+    private Label successfulSignUpLabel;
+
     private Student currentStudent;
 
     @FXML
     private void onSubmitClicked(){
         String name = nameField.getText();
+
+        String ageInput = ageField.getText().trim();
         int age;
-        age = Integer.parseInt(ageField.getText());
+        try {
+            age = Integer.parseInt(ageInput);
+        } catch (NumberFormatException e) {
+            successfulSignUpLabel.setText("Age must be a valid number.");
+            successfulSignUpLabel.setVisible(true);
+            return;
+        }
+
         String email = emailField.getText();
         String password = passwordField.getText();
 
         currentStudent = new Student(name, age, email, password);
 
-        System.out.println("Created Student: " + currentStudent.getName());
+        successfulSignUpLabel.setText("Successful Student Signup! Welcome " + name);
+        successfulSignUpLabel.setVisible(true);
 
         // TODO: Error handling for incorrect user inputs
     }
+
+
 }
