62a50e3e64e718d7405df97ad3a108ded5d9a111
Pragati Vijayakumar <n11171499@qut.edu.au>
2025-04-20 20:26:16 +1000

    Adding a switch-case to associate input data into a new instance of that specific user when a user is registered.

--- src/main/java/org/jaspr/hr/demo/RegisterController.java
+++ src/main/java/org/jaspr/hr/demo/RegisterController.java
@@ -49,6 +49,31 @@ public class RegisterController {
     @FXML
     private TextField passwordFieldParent;
 
+    @FXML
+    private TextField nameFieldAdmin;
+    @FXML
+    private TextField ageFieldAdmin;
+    @FXML
+    private TextField adminIDField;
+    @FXML
+    private TextField emailFieldAdmin;
+    @FXML
+    private TextField passwordFieldAdmin;
+
+    @FXML
+    private Label successfulSignUpLabelStudent;
+    @FXML
+    private Label successfulSignUpLabelTeacher;
+    @FXML
+    private Label successfulSignUpLabelParent;
+    @FXML
+    private Label successfulSignUpLabelAdmin;
+
+
+    private Student newStudent;
+    private Teacher newTeacher;
+    private Parent newParent;
+    private Admin newAdmin;
 
     public void initialize(){
         roleComboBox.setItems(FXCollections.observableArrayList("Student", "Teacher", "Parent", "Admin"));
@@ -72,6 +97,72 @@ public class RegisterController {
 
     }
 
+        @FXML
+        private void onSubmitClicked(){
+            String role = roleComboBox.getValue();
+            String name, email, password;
+            int age = 0;
+
+            switch (role){
+                case "Student" -> {
+                    name = nameFieldStudent.getText();
+                    email = emailFieldStudent.getText();
+                    password = passwordFieldStudent.getText();
+                    age = Integer.parseInt(ageFieldStudent.getText().trim());
+                    int studentID = Integer.parseInt(studentIDField.getText().trim());
+
+                    Student newStudent = new Student(name, age, studentID, email, password);
+                    successfulSignUpLabelStudent.setText("Successful Student Registration! Welcome " + name + "!");
 
 
-}
+
+                }
+                case "Teacher" -> {
+                    name = nameFieldTeacher.getText();
+                    email = emailFieldTeacher.getText();
+                    password = passwordFieldTeacher.getText();
+                    age = Integer.parseInt(ageFieldTeacher.getText().trim());
+                    int teacherID = Integer.parseInt(teacherIDField.getText().trim());
+
+                    Teacher newTeacher = new Teacher(name, age, teacherID, email, password);
+                    successfulSignUpLabelTeacher.setText("Successful Teacher Registration! Welcome " + name + "!");
+
+
+                }
+                case "Parent" -> {
+                    name = nameFieldParent.getText();
+                    String child = childNameField.getText();
+                    int childID = Integer.parseInt(childIDField.getText().trim());
+                    email = emailFieldParent.getText();
+                    password = passwordFieldParent.getText();
+
+                    Parent newParent = new Parent(name, child, childID, email, password);
+                    successfulSignUpLabelParent.setText("Successful Parent Registration! Welcome " + name + "!");
+
+
+
+                }
+                case "Admin" -> {
+                    name = nameFieldAdmin.getText();
+                    email = emailFieldAdmin.getText();
+                    password = passwordFieldAdmin.getText();
+                    age = Integer.parseInt(ageFieldAdmin.getText().trim());
+                    int adminID = Integer.parseInt(adminIDField.getText().trim());
+
+                    Admin newAdmin = new Admin(name, age, adminID, email, password);
+                    successfulSignUpLabelAdmin.setText("Successful Administrator Registration! Welcome " + name + "!");
+
+//                    successfulSignUpLabel.setVisible(true);
+
+                    // TODO: Error handling for incorrect user inputs
+                }
+            }
+
+
+
+        }
+
+
+    }
+
+
