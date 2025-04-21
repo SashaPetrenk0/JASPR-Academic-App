274cb6bd733fa1fc58349b49f2d54c26cef1f46f
Jamie-C11 <jamie.corrie@connect.qut.edu.au>
2025-04-21 16:25:26 +1000

    Initialize window backdrop at program start. This was a fix to resolve an error of window not rendering properly previously.

--- src/main/java/org/jaspr/hr/demo/DashboardController.java
+++ src/main/java/org/jaspr/hr/demo/DashboardController.java
@@ -1,5 +1,16 @@
 package org.jaspr.hr.demo;
 
-public class DashboardController {
+import javafx.fxml.FXML;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 
+public class DashboardController {
+    @FXML
+    private VBox vBox;
+
+    @FXML
+    public void initialize() {
+        // Set background color to light blue
+        vBox.setStyle("-fx-background-color: lightblue;");
+    }
 }
