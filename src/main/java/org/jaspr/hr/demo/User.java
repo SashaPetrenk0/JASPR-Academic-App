9808011d5757df99cb39a15c47b10f13eb2bd471
Pragati Vijayakumar <n11171499@qut.edu.au>
2025-04-21 11:30:51 +1000

    Update user from interface to a abstract class with added role attribute

--- src/main/java/org/jaspr/hr/demo/User.java
+++ src/main/java/org/jaspr/hr/demo/User.java
@@ -1,7 +1,35 @@
 package org.jaspr.hr.demo;
 
-public interface User {
-    String getName();
-    String getEmail();
-    String getPassword();
+public abstract class User {
+   protected String name;
+   protected String email;
+   protected String password;
+   protected String role;
+
+   public User(String name, String email, String password){
+       this.name = name;
+       this.email = email;
+       this.password = password;
+       this.role = getRole();
+   }
+
+   public abstract String getRole();
+
+   public String getName()
+   {
+       return name;
+   }
+
+    public String getEmail()
+    {
+        return email;
+    }
+    public String getPassword()
+    {
+        return password;
+    }
+    public String getRoleValue()
+    {
+        return role;
+    }
 }
