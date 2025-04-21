f5b4bdda60a428f24ef5bc3ba1d3a13287be53d3
Pragati Vijayakumar <n11171499@qut.edu.au>
2025-04-21 13:07:17 +1000

    Temporarily redirect login button to registration page until login page is created.

--- src/main/java/org/jaspr/hr/demo/HelloController.java
+++ src/main/java/org/jaspr/hr/demo/HelloController.java
@@ -3,9 +3,6 @@ package org.jaspr.hr.demo;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.CheckBox;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
@@ -24,7 +21,7 @@ public class HelloController {
     @FXML
     protected void onLoginButtonClick() throws IOException {
         Stage stage = (Stage) loginButton.getScene().getWindow();
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("student-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("register-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), HelloApplication.WIDTH, HelloApplication.HEIGHT);
         stage.setScene(scene);
     }
