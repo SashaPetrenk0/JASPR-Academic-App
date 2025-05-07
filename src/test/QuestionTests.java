587d4b30703103f57fe65b39238987bc30676b48
awr3n <aswre000@gmail.com>
2025-05-08 09:38:06 +1000

    Started test skeleton, added Pragati's file for reference

--- src/test/QuestionTests.java
+++ src/test/QuestionTests.java
@@ -1,2 +1,16 @@
+import org.jaspr.hr.demo.*;
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+import java.sql.*;
+import java.util.List;
+
+
 public class QuestionTests {
+    private static Question question;
+
+
+    @BeforeEach
+    public void setup() {
+
+    }
 }
