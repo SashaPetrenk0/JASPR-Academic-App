30378655995c0bf7e548d28f4bc194b1b499c091
Sasha <sasha.petrenk6@gmail.com>
2025-05-09 11:45:48 +1000

    create methods for IResultsDAO

--- src/main/java/org/jaspr/hr/demo/IResultsDAO.java
+++ src/main/java/org/jaspr/hr/demo/IResultsDAO.java
@@ -1,4 +1,14 @@
 package org.jaspr.hr.demo;
 
 public interface IResultsDAO {
+
+     void addResult(int studentID, int id, int questionId, boolean correct);
+
+     void getResultsByQuestion(int questionId, boolean correct);
+
+     void getResultsByQuiz(int quizId);
+
+
+
+
 }
