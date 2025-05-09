8156c5eb595ae75141b115d3da7309568f30be5f
Sasha <sasha.petrenk6@gmail.com>
2025-05-09 11:48:14 +1000

    create SqliteResultsDAO

---
+++ src/main/java/org/jaspr/hr/demo/SqliteResultsDAO.java
@@ -0,0 +1,18 @@
+package org.jaspr.hr.demo;
+
+public class SqliteResultsDAO implements IResultsDAO {
+    @Override
+    public void addResult(int studentID, int id, int questionId, boolean correct) {
+
+    }
+
+    @Override
+    public void getResultsByQuestion(int questionId, boolean correct) {
+
+    }
+
+    @Override
+    public void getResultsByQuiz(int quizId) {
+
+    }
+}
