a772df863674491b7c338ab80ad1880daf7f99f5
Pragati Vijayakumar <n11171499@qut.edu.au>
2025-05-11 21:52:07 +1000

    Apply these changes in other classes that use this method

--- src/main/java/org/jaspr/hr/demo/QuizAssignmentDetailsController.java
+++ src/main/java/org/jaspr/hr/demo/QuizAssignmentDetailsController.java
@@ -1,4 +1,31 @@
 package org.jaspr.hr.demo;
 
+import javafx.beans.property.BooleanProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.collections.ObservableMap;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+
+import java.util.List;
+
 public class QuizAssignmentDetailsController {
+
+    @FXML
+    private ListView<String> classroomListView;
+
+    private ObservableList<String> classroomNames = FXCollections.observableArrayList();
+    private ObservableMap<String, BooleanProperty> selectedClassrooms = FXCollections.observableHashMap();
+
+    private final SqliteUserDAO userDAO = new SqliteUserDAO();
+
+    private Teacher currentTeacher;
+
+    public void setTeacher(Teacher teacher){
+        this.currentTeacher = teacher;
+    }
+
+    private void loadClassrooms(){
+        List<Integer> classrooms = userDAO.getClassroomNumberForTeacher(currentTeacher.getTeacherID());
+    }
 }
