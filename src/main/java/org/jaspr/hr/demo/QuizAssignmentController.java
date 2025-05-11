d29560e5934240885b8a6cdabd31fa6ffbe63cea
Pragati Vijayakumar <n11171499@qut.edu.au>
2025-05-12 09:41:35 +1000

    Create method to handle quiz clicked (redirect to separate assignment page).

--- src/main/java/org/jaspr/hr/demo/QuizAssignmentController.java
+++ src/main/java/org/jaspr/hr/demo/QuizAssignmentController.java
@@ -1,43 +1,54 @@
 package org.jaspr.hr.demo;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.ListView;
 import javafx.stage.Stage;
 
 import java.awt.event.MouseEvent;
+import java.io.IOException;
 import java.util.List;
+import javafx.scene.Parent;
 
 
 public class QuizAssignmentController {
 
     @FXML
-    private ListView<String> quizListView;
+    private ListView<Quiz> quizListView;
 
-    private Teacher currentTeacher;
+    // Store retrieved quizzes
+    private List<Quiz> quizzes;
+
+//    private Teacher currentTeacher;
 
     private final SqliteQuizDAO quizDAO = new SqliteQuizDAO();
 
     public void setTeacher(Teacher teacher){
-        this.currentTeacher = teacher;
-    }
-
-    private void loadQuizzes(){
-        List<String> quizzes = quizDAO.getAllQuizzes(currentTeacher);
+        quizzes = quizDAO.getAllQuizzes(teacher);
         quizListView.getItems().setAll(quizzes);
+//        this.currentTeacher = teacher;
     }
 
     @FXML
-    private void handleQuizClicked(MouseEvent event){
+    private void handleQuizClicked(MouseEvent event) throws IOException {
         if(event.getClickCount() == 1){
-            String selectedQuiz = quizListView.getSelectionModel().getSelectedItem();
+            Quiz selectedQuiz = quizListView.getSelectionModel().getSelectedItem();
             if (selectedQuiz != null){
                 goToQuiz(selectedQuiz);
             }
         }
     }
 
-    private void goToQuiz(String quizTitle){
+    private void goToQuiz(Quiz quiz) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/org/jaspr/hr/demo/quiz-assignment-details.fxml"));
+        Parent root = loader.load();
+
+        QuizAssignmentDetailsController controller = loader.getController();
+        controller.setQuizID(quiz); // <--- Pass quiz object
+
         Stage stage = (Stage) quizListView.getScene().getWindow();
-        SceneChanger.changeScene(stage, "quiz-assignment-details.fxml");
+        stage.setScene(new Scene(root));
+
     }
 }
