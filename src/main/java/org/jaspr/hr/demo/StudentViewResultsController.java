f9781e3d3172bc0dd03657eee2d8d6978d108729
Sasha <sasha.petrenk6@gmail.com>
2025-05-17 19:19:23 +1000

    setting up controller to display different results

--- src/main/java/org/jaspr/hr/demo/StudentViewResultsController.java
+++ src/main/java/org/jaspr/hr/demo/StudentViewResultsController.java
@@ -5,22 +5,25 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.scene.control.*;
 import javafx.collections.FXCollections;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class StudentViewResultsController {
     private final User user = UserSession.getInstance().getCurrentUser();
     private final String role = UserSession.getInstance().getRole();
 
     private final SqliteQuizDAO quizDAO = new SqliteQuizDAO();
-
+    private final SqliteResultsDAO resultsDAO = new SqliteResultsDAO();
 
     @FXML
     private ComboBox resultDropdown;
+    Student student = (Student) user;
 
 
 
@@ -44,7 +47,32 @@ public class StudentViewResultsController {
     }
 
     @FXML
-    private void onQuizSelected(){
+    private void onQuizSelected() {
 
+        // First hide everything
+        allResultsBox.setVisible(false);
+        specificResultsBox.setVisible(false);
+
+        // Then show the selected one
+        String quiz = (String) resultDropdown.getValue();
+        if (quiz == "All quizzes"){
+            showOnlyResults(allResultsBox);
+            List<Map<String, Integer>> quizResults = resultsDAO.getResultsByQuiz(student.getStudentID());
+        }
+        else{
+            showOnlyResults(specificResultsBox);
+        }
+    }
+
+    private void showOnlyResults(VBox boxToShow) {
+
+        allResultsBox.setVisible(false);
+        allResultsBox.setManaged(false);
+
+        specificResultsBox.setVisible(false);
+        specificResultsBox.setManaged(false);
+
+        boxToShow.setVisible(true);
+        boxToShow.setManaged(true);
     }
 }
