6b163df40da5ef2805ebb4698452cd27023a907d
Sasha <sasha.petrenk6@gmail.com>
2025-06-10 12:04:47 +1000

    commenting

--- src/main/java/org/jaspr/hr/demo/ResponseListener.java
+++ src/main/java/org/jaspr/hr/demo/ResponseListener.java
@@ -1,5 +1,13 @@
 package org.jaspr.hr.demo;
 
+/**
+ * A listener interface for receiving responses from an asynchronous process,
+ * waiting for the ollama LLM to generate a response
+ */
 public interface ResponseListener {
+     /**
+      * Called when a response is recieved from the ollama
+      * @param response the response from the LLM
+      */
      void onResponseReceived(OllamaResponse response);
 }
